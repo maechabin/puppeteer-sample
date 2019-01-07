ef48bea11056e98f3f2ec96d0e973ad1828d01c9
maechabin <mitoppo@gmail.com>
2019-01-07 23:05:49 +0900

    first commit

---
+++ screenshot.js
@@ -0,0 +1,10 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const TARGET_URL = 'https://mae.chab.in';
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(TARGET_URL);
+  await page.screenshot({ path: 'example.png' });
+  await browser.close();
+})();
