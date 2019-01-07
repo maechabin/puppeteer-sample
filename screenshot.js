21f735b699b33c768dee613fabfe10604d2e1aa9
maechabin <mitoppo@gmail.com>
2019-01-08 01:32:21 +0900

    google検索の自動化

--- screenshot.js
+++ screenshot.js
@@ -1,10 +1,38 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const TARGET_URL = 'https://mae.chab.in';
-  const browser = await puppeteer.launch();
+  const TARGET_URL = 'https://www.google.co.jp';
+
+  /** puppeteerを起動 */
+  const browser = await puppeteer.launch({
+    headless: false, // Headlessモードで起動するかどうか
+    slowMo: 50, // 50ミリ秒スローモーションで実行する
+  });
+
+  /** 新しい空のページを開く */
   const page = await browser.newPage();
+
+  /** view portの設定 */
+  await page.setViewport({
+    width: 1200,
+    height: 800,
+  });
+
+  /** Webページにアクセスする */
   await page.goto(TARGET_URL);
-  await page.screenshot({ path: 'example.png' });
+
+  /** フォームにテキストを入力する */
+  await page.type('input[name=q]', 'Puppeteer');
+
+  /** Webページのスクリーンショットを撮る */
+  await page.screenshot({ path: './screenshot/example.png' });
+
+  /** ボタンをクリックする */
+  await page.click('input[name=btnK]');
+
+  /** Webページのスクリーンショットを撮る */
+  await page.screenshot({ path: './screenshot/example2.png' });
+
+  /** 手動でブラウザを終了する */
   await browser.close();
 })();
